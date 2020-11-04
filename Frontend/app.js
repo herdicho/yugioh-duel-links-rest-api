8fed023361b7af40e100686fee84755eeb0930f8
Farras Herdicho <you@example.com>
2020-11-04 14:20:55 +0700

    add id character to every row and add class for character that already on max lv

--- Frontend/app.js
+++ Frontend/app.js
@@ -15,10 +15,15 @@ const loadCharactersListbyWorld = world => {
     fetch(api).
     then(res => res.json()).
     then(res => res.payload.forEach(element => {
-        html = '<tr> <td>%name%</td> <td>%curLv%</td> <td>%maxLv%</td> <th><i class="fas fa-marker"></i></th> <th><i class="fas fa-trash"></i></th> </tr>'
+        if (element.maxLv !== element.currentLv)
+            html = '<tr id="%id%"> <td>%name%</td> <td>%curLv%</td> <td>%maxLv%</td> <th><i class="fas fa-marker"></i></th> <th><i class="fas fa-trash"></i></th> </tr>'
+        else
+            html = '<tr id="%id%"> <td class="max-lvl">%name%</td> <td class="max-lvl">%curLv%</td> <td class="max-lvl">%maxLv%</td> <th class="max-lvl"><i class="fas fa-marker"></i></th> <th class="max-lvl"><i class="fas fa-trash"></i></th> </tr>'
+        
         newHtml = html.replace('%name%', element.name)
         newHtml = newHtml.replace('%curLv%', element.currentLv)
         newHtml = newHtml.replace('%maxLv%', element.maxLv)
+        newHtml = newHtml.replace('"%id%"', element.id)
         elementByWorld.insertAdjacentHTML('afterend', newHtml);
     }))
 } 
